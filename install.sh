531ec4991c84c5c6c2ac9701fb0dd2a6d1bc7b72
Jan Hartman <jhartman@outbrain.com>
2020-09-01 14:12:12 +0200

    Remove unused script

--- install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -eo pipefail
-
-python3 -m pip install --user virtualenv
-virtualenv --python=python3 recsystf_venv
-source recsystf_venv/bin/activate
-pip install -r requirements.txt
